a5286a5df1a2c301158bd1ad0bebb2d1a3f50cb2
Shannon Besson <shannon0413@outlook.com>
2025-05-20 12:34:40 +0200

    (feat(Migrations):Création de mes tables grâce aux migrations)

--- back/database/migrations/2025_05_20_102759_users.php
+++ back/database/migrations/2025_05_20_102759_users.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'intervenant', 'etudiant']);
+            $table->foreignId('classe_id')->nullable()->constrained()->onDelete('set null');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ back/database/migrations/2025_05_20_103201_cours.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cours', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->foreignId('classe_id')->constrained('classes')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('intervenant_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('salle')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->dateTime('date');
+            $table->dateTime('h_debut');
+            $table->dateTime('h_fin');
+            $table->integer('duree');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cours');
+    }
+};
